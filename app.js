e844cf05b6f19ac57bc7dff03154c2032d9e2eec
WaseemIshaq44 <waseemishaq44@gmail.com>
2025-01-10 16:09:01 +0500

    some issues solved

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 function checkBloodGroup() {
-    const bloodGroup = document.getElementById('bloodGroup').value.toLowerCase();
-    let compatibleGroups = [];
-    console.log(bloodGroup);
-    if(bloodGroup=='a+'){
-        compatibleGroups = ['A+', 'A-', 'O+', 'O-']
-    }else if(bloodGroup=='a-'){
-        compatibleGroups = ['A-', 'O-'];
-    }else if('a-'){
-    compatibleGroups = ['A-', 'O-'];
-    }else if('b+'){
-    compatibleGroups = ['B+', 'B-', 'O+', 'O-'];
-    }
-    else if('b-'){
-    compatibleGroups = ['B-', 'O-'];
-    }
-    else if('ab+'){
-    compatibleGroups = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
-    }
-    else if('ab-'){
-    compatibleGroups = ['A-', 'B-', 'AB-', 'O-'];
-    }
-    else if('o+'){
-    compatibleGroups = ['O+', 'O-'];
-    }
-    else if('o-'){
-    compatibleGroups = ['O-'];
+    const bloodGroup = document.getElementById('bloodGroup').value.toUpperCase();
+    let compatibleGroups = "";
+    if(bloodGroup=='A+'){
+        compatibleGroups = "A+, A-, O+, O-"
+    }else if(bloodGroup=='A-'){
+    compatibleGroups = "A-,O-";
+    }else if(bloodGroup=='B+'){
+    compatibleGroups = "B+, B-, O+, O-";
+    }else if(bloodGroup=='B-'){
+    compatibleGroups = "B-, O-";
+    }else if(bloodGroup=='AB+'){
+    compatibleGroups = "A+, A-, B+, B-, AB+, AB-, O+, O-";
+    }else if(bloodGroup=='AB-'){
+    compatibleGroups = "A-, B-, AB-, O-";
+    }else if(bloodGroup=='O+'){
+    compatibleGroups = "O+, O-";
+    }else if(bloodGroup=='O-'){
+    compatibleGroups = "O-";
     }else{
-        compatibleGroups = ['Invalid blood group. Please enter a valid blood group.'];
+        compatibleGroups = "Invalid blood group. Please enter a valid blood group.'";
     }
-    document.getElementById('result').innerText = compatibleGroups.join(', ');
+    document.getElementById('result').innerText = compatibleGroups;
 }
